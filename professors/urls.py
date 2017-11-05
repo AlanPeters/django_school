eec800939551481983082f22a59202fb33497d5d
Alan Peters <alan3051@gmail.com>
2017-11-05 09:41:48 -0700

    Adding basic professor templates

--- professors/urls.py
+++ professors/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.professors, name='index'),
     url(r'professors$', views.professors, name='professors'),
+    url(r'^(?P<professor_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- professors/views.py
+++ professors/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from .models import Professor
 # Create your views here.
 
 def professors(request):
-    return HttpResponse("Professosrs")
+    professors_list = Professor.objects.order_by('-name')
+    context = {
+        'professors_list': professors_list,
+    }
+    return render(request, 'professors/index.html', context)
+
+def detail(request, professor_id):
+    professor = get_object_or_404(Professor, pk=professor_id)
+    sections = professor.section_set.all()
+    return render(request, 'professors/detail.html', {'professor': professor, 'sections': sections})
+    
+
+
