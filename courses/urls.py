a23020eccdcef62e974598052a179a3917e8f1fa
Alan Peters <alan3051@gmail.com>
2017-11-05 10:00:18 -0700

    Adding basic views and routes for courses

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.courses, name='index'),
     url(r'courses$', views.courses, name='courses'),
+    url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
 ]
